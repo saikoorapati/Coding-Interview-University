7492f89db4d7132d4fe1ff013827835e8a31bb1a
Sai Koorapati <saikoorapati@gmail.com>
2019-05-11 00:08:53 -0700

    BFS & DFS

---
+++ BFSandDFS/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace BFSandDFS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BFS
+            BreadthFirstAlgorithm b = new BreadthFirstAlgorithm();
+            Employee root = b.BuildEmployeeGraph();
+            Console.WriteLine("Traverse Graph\n------");
+            b.Traverse(root);
+
+            Console.WriteLine("\nSearch in Graph\n------");
+            Employee e = b.Search(root, "Eva");
+            Console.WriteLine(e == null ? "Employee not found" : e.name);
+            e = b.Search(root, "Brian");
+            Console.WriteLine(e == null ? "Employee not found" : e.name);
+            e = b.Search(root, "Soni");
+            Console.WriteLine(e == null ? "Employee not found" : e.name);
+
+            //DFS
+            DepthFirstAlgorithm d = new DepthFirstAlgorithm();
+            Employee rootD = d.BuildEmployeeGraph();
+            Console.WriteLine("Traverse Graph\n------");
+            d.Traverse(rootD);
+
+            Console.WriteLine("\nSearch in Graph\n------");
+            Employee emp = d.Search(root, "Eva");
+            Console.WriteLine(emp == null ? "Employee not found" : emp.name);
+            emp = d.Search(root, "Brian");
+            Console.WriteLine(emp == null ? "Employee not found" : emp.name);
+            emp = d.Search(root, "Soni");
+            Console.WriteLine(emp == null ? "Employee not found" : emp.name);
+        }
+
+        public class Employee
+        {
+            public string name { get; set; }
+            public Employee(string name)
+            {
+                this.name = name;
+            }
+
+            System.Collections.Generic.List<Employee> EmployeeList = new System.Collections.Generic.List<Employee>();
+
+            public void isEmployeeOf(Employee p)
+            {
+                EmployeeList.Add(p);
+            }
+
+            public override string ToString()
+            {
+                return name;
+            }
+
+            public List<Employee> Employees
+            {
+                get
+                {
+                    return EmployeeList;
+                }
+            }
+        }
+
+        public class BreadthFirstAlgorithm
+        {
+            public Employee BuildEmployeeGraph()
+            {
+                Employee EVA = new Employee("Eva");
+                Employee Eva = new Employee("Eva");
+                Employee Sophia = new Employee("Sophia");
+                Employee Brian = new Employee("Brian");
+                Eva.isEmployeeOf(Sophia);
+                Eva.isEmployeeOf(Brian);
+
+                Employee Lisa = new Employee("Lisa");
+                Employee Tina = new Employee("Tina");
+                Employee John = new Employee("John");
+                Employee Mike = new Employee("Mike");
+                Sophia.isEmployeeOf(Lisa);
+                Sophia.isEmployeeOf(John);
+                Brian.isEmployeeOf(Tina);
+                Brian.isEmployeeOf(Mike);
+
+                return Eva;
+            }
+
+            public void Traverse(Employee root)
+            {
+                Queue<Employee> TraveralOrder = new Queue<Employee>();
+
+                Queue<Employee> Q = new Queue<Employee>();
+                HashSet<Employee> S = new HashSet<Employee>();
+
+                Q.Enqueue(root);
+                S.Add(root);
+
+                while(Q.Count > 0)
+                {
+                    Employee e = Q.Dequeue();
+                    TraveralOrder.Enqueue(e);
+
+                    foreach(Employee emp in e.Employees)
+                    {
+                        if(!S.Contains(emp))
+                        {
+                            Q.Enqueue(emp);
+                            S.Add(emp);
+                        }
+                    }
+                }
+                while (TraveralOrder.Count > 0)
+                {
+                    Employee e = TraveralOrder.Dequeue();
+                    Console.WriteLine(e);
+                }
+            }
+            public Employee Search(Employee root, string nameToSearchFor)
+            {
+                Queue<Employee> Q = new Queue<Employee>();
+                HashSet<Employee> S = new HashSet<Employee>();
+                Q.Enqueue(root);
+                S.Add(root);
+
+                while (Q.Count > 0)
+                {
+                    Employee e = Q.Dequeue();
+                    if (e.name == nameToSearchFor)
+                        return e;
+                    foreach (Employee friend in e.Employees)
+                    {
+                        if (!S.Contains(friend))
+                        {
+                            Q.Enqueue(friend);
+                            S.Add(friend);
+                        }
+                    }
+                }
+                return null;
+            }
+        }
+
+        public class DepthFirstAlgorithm
+        {
+            public Employee BuildEmployeeGraph()
+            {
+                Employee Eva = new Employee("Eva");
+                Employee Sophia = new Employee("Sophia");
+                Employee Brian = new Employee("Brian");
+                Eva.isEmployeeOf(Sophia);
+                Eva.isEmployeeOf(Brian);
+
+                Employee Lisa = new Employee("Lisa");
+                Employee Tina = new Employee("Tina");
+                Employee John = new Employee("John");
+                Employee Mike = new Employee("Mike");
+                Sophia.isEmployeeOf(Lisa);
+                Sophia.isEmployeeOf(John);
+                Brian.isEmployeeOf(Tina);
+                Brian.isEmployeeOf(Mike);
+
+                return Eva;
+            }
+
+            public void Traverse(Employee root)
+            {
+                Console.WriteLine(root.name);
+
+                for(int i=0; i<root.Employees.Count;i++)
+                {
+                    Traverse(root.Employees[i]);
+                }
+
+            }
+            public Employee Search(Employee root, string nameToSearchFor)
+            {
+                if (nameToSearchFor == root.name)
+                    return root;
+
+                Employee personFound = null;
+                for (int i = 0; i < root.Employees.Count; i++)
+                {
+                    personFound = Search(root.Employees[i], nameToSearchFor);
+                    if (personFound != null)
+                        break;
+                }
+                return personFound;
+            }
+        }
+        }
+}
--- BinaryTrees/Program.cs
+++ BinaryTrees/Program.cs
@@ -7,7 +7,122 @@ namespace BinaryTrees
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Tree BST = new Tree();
+            BST.Insert(30);
+            BST.Insert(35);
+            BST.Insert(57);
+            BST.Insert(15);
+            BST.Insert(63);
+            BST.Insert(49);
+            BST.Insert(89);
+            BST.Insert(77);
+            BST.Insert(67);
+            BST.Insert(98);
+            BST.Insert(91);
+            Console.WriteLine("Inorder Traversal : ");
+            BST.InOrder(BST.ReturnRoot());
+            Console.WriteLine(" ");
+            Console.WriteLine();
+            Console.WriteLine("Preorder Traversal : ");
+            BST.PreOrder(BST.ReturnRoot());
+            Console.WriteLine(" ");
+            Console.WriteLine();
+            Console.WriteLine("Postorder Traversal : ");
+            BST.PostOrder(BST.ReturnRoot());
+            Console.WriteLine(" ");
+            Console.ReadLine();
+        }
+    }
+
+    class Node
+    {
+        public int item;
+        public Node right;
+        public Node left;
+        public void display()
+        {
+            Console.Write("[");
+            Console.Write(item);
+            Console.Write("]");
+        }
+    }
+
+    class Tree
+    {
+        public Node root;
+        public Tree ()
+        {
+            root = null;
+        }
+
+        public Node ReturnRoot()
+        {
+            return root;
+        }
+
+        public void Insert(int id)
+        {
+            Node newNode = new Node();
+            newNode.item = id;
+            if (root == null)
+                root = newNode;
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (id < current.item)
+                    {
+                        current = current.left;
+                        if (current == null)
+                        {
+                            parent.left = newNode;
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        current = current.right;
+                        if (current == null)
+                        {
+                            parent.right = newNode;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void PreOrder(Node root)
+        {
+            if(root != null)
+            {
+                Console.WriteLine(root.item + "");
+                PreOrder(root.left);
+                PreOrder(root.right);
+            }
+        }
+
+        public void InOrder(Node root)
+        {
+            if (root != null)
+            {
+                PreOrder(root.left);
+                Console.WriteLine(root.item + "");
+                PreOrder(root.right);
+            }
+        }
+
+        public void PostOrder(Node root)
+        {
+            if (root != null)
+            {
+                PreOrder(root.left);
+                PreOrder(root.right);
+                Console.WriteLine(root.item + "");
+            }
         }
     }
 }
